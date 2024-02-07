2ab748293aebd91fd835aba8ac69a1c7045bcabe
Hyperxq <barrientosd9@gmail.com>
2024-02-07 11:26:21 -0600

    Auto-generated commit

--- chunk-XAWJSWML.js
+++
@@ -1,1 +0,0 @@
-import{Aa as s,R as o,pa as i,qa as a,xa as r}from"./chunk-37LVPKMS.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-home"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(i(0,"p"),r(1,"home works!"),a())}});let t=e;return t})();export{d as HomeComponent};
