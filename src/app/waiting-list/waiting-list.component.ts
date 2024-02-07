29c91b91861611e42840fc744f9ae281caa68a5d
Hyperxq <barrientosd9@gmail.com>
2024-02-06 23:10:14 -0600

    feat: waiting list pixel perfect, no responsive

--- src/app/waiting-list/waiting-list.component.ts
+++ src/app/waiting-list/waiting-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-waiting-list',
@@ -7,22 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './waiting-list.component.html',
   styleUrl: './waiting-list.component.scss',
 })
-export class WaitingListComponent {
+export class WaitingListComponent implements OnInit {
+  @ViewChild('video', { static: false }) video!: ElementRef<HTMLVideoElement>;
+  videoPlaying = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  async togglevideo() {
+    if (!this.videoPlaying) {
+      await this.video.nativeElement.play();
+      await this.video.nativeElement.requestFullscreen();
+      this.videoPlaying = true;
+    } else {
+      this.video.nativeElement.pause();
+      this.videoPlaying = false;
+    }
+  }
+
   toggleTheme() {
     const theme =
       document.documentElement.getAttribute('data-theme') === 'dark'
         ? 'light'
         : 'dark';
     document.documentElement.setAttribute('data-theme', theme);
-
-    // Update base variables to reflect the current theme
-    document.documentElement.style.setProperty(
-      '--color-background',
-      `var(--color-background-${theme})`,
-    );
-    document.documentElement.style.setProperty(
-      '--color-text',
-      `var(--color-text-${theme})`,
-    );
   }
 }
