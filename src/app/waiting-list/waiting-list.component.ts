e43c9bba457c0ff76bd62cf587b168ee701b17fb
Hyperxq <barrientosd9@gmail.com>
2024-02-06 18:37:05 -0600

    chore: Reverting figma subproject creation and adding css variables

--- src/app/waiting-list/waiting-list.component.ts
+++ src/app/waiting-list/waiting-list.component.ts
@@ -5,8 +5,24 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './waiting-list.component.html',
-  styleUrl: './waiting-list.component.scss'
+  styleUrl: './waiting-list.component.scss',
 })
 export class WaitingListComponent {
+  toggleTheme() {
+    const theme =
+      document.documentElement.getAttribute('data-theme') === 'dark'
+        ? 'light'
+        : 'dark';
+    document.documentElement.setAttribute('data-theme', theme);
 
+    // Update base variables to reflect the current theme
+    document.documentElement.style.setProperty(
+      '--color-background',
+      `var(--color-background-${theme})`,
+    );
+    document.documentElement.style.setProperty(
+      '--color-text',
+      `var(--color-text-${theme})`,
+    );
+  }
 }
